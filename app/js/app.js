520e443dbbcbfc0ad65dcb863661a07d5ca28c06
Unknown <grevtsov85@mail.ru>
2021-10-17 22:18:51 +0500

    Добавление проекта нердс

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+// import vendor jQuery plugin example
+// import 'app/libs/jquery/jquery.min.js'
+import '~/app/libs/slick/slick.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	// ниже пишем код
+
+	$('.slider__wrapper').slick();
+
+})
